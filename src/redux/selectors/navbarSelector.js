b2e2369144806318da940c4c9af7199f06d7b15a
Богданов Кирилл <bogdanovka@data-platform.ru>
2022-06-19 21:33:53 +0300

    add back-end. refactor ui api for authorization

--- src/redux/selectors/navbarSelector.js
+++
@@ -1,8 +0,0 @@
-import {createSelector} from "reselect";
-
-const getFriendsSelector = (state) => {
-    return state.navbar.friends
-};
-export const getFriendsS = createSelector(getFriendsSelector, (friends) => {
-    return friends;
-})
